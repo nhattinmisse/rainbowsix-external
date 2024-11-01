94ecc07f6d437243ebf8b003c9892ff91ccb4a89
nhattinmisse <154442176+nhattinmisse@users.noreply.github.com>
2024-11-02 02:39:11 +0700

    update file

---
+++ R6S-External-Cheat/GameManager.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+
+#include "GameManager.h"
+#include "Offsets.h"
+#include "Memory.h"
+
+namespace Engine
+{
+	Array<Entity*> GameManager::GetEntities()
+	{
+		return *Memory::Ptr<Array<Entity*>*>(this, OFFSET_GAMEMANAGER_ENTITYLIST);
+	}
+
+	GameManager* GameManager::GetInstance()
+	{
+		return *reinterpret_cast<GameManager**>(ADDRESS_GAMEMANAGER + reinterpret_cast<uint64_t>(GetModuleHandle(NULL)));
+	}
+}
