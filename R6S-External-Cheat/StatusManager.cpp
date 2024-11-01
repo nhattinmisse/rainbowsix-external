0c448a949ce867f95576bec3575657e5d2b3f83d
nhattinmisse <154442176+nhattinmisse@users.noreply.github.com>
2024-11-02 02:39:39 +0700

    update file

---
+++ R6S-External-Cheat/StatusManager.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include "Offsets.h"
+#include "Memory.h"
+#include "StatusManager.h"
+
+namespace Engine
+{
+
+	StatusManager* StatusManager::GetInstance()
+	{
+		return *reinterpret_cast<StatusManager**>(ADDRESS_STATUSMANAGER + reinterpret_cast<uint64_t>(GetModuleHandle(NULL)));
+	}
+
+	bool StatusManager::IsIngame()
+	{
+		auto p0 = *Memory::Ptr<int*>(this, OFFSET_ISINGAME);
+
+		if (p0 == 1)
+			return true;
+		else
+			return false;
+	}
+}
